68eb5abc2ab0ac10f11d2babbdaaba70a102020f
Maafi Afroz <mahmudaafroz109@gmail.com>
2024-12-06 20:19:02 +0600

    js-recap-for-reactjs

---
+++ js-recap-for-reactjs/1-fundamentals.js
@@ -0,0 +1,20 @@
+// 1. how to declare a variable using let and const
+// avoid variale declaration using var
+const month = 'December';
+let season = 'winter';
+season = spring;
+
+// 2. conditions
+// 6 basic conditions: >, <, ===, !==, <==, >==
+// 2 multiple conditions: &&, ||
+if(month === 'December' && season === 'spring'){
+    console.log('december in winter');
+} else if(month === 'December'){
+    console.log('only december');
+} else {
+
+}
+
+// 3. array declarations
+const years = [2023, 2024, 2025];
+years
